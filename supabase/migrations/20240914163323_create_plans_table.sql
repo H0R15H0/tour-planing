8fedc36a6b99026b3bc4a093945576984b0dd830
H0R15H0 <shohori13@gmail.com>
2024-09-15 04:53:08 +0900

    Add timestamp columns

--- supabase/migrations/20240914163323_create_plans_table.sql
+++ supabase/migrations/20240914163323_create_plans_table.sql
@@ -1,5 +1,7 @@
 CREATE TABLE plans (
   id UUID PRIMARY KEY,
   user_id UUID NOT NULL REFERENCES auth.users(id),
-  name TEXT NOT NULL
+  name TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT (now() at time zone 'JST'),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT (now() at time zone 'JST')
 );
